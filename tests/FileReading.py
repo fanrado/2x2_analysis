5041d03a883368385b1023c3171b7a4cf1665744
fanrado <radofana@gmail.com>
2024-12-22 23:58:02 -0500

    Update for testing the file reading script

--- tests/FileReading.py
+++ tests/FileReading.py
@@ -15,8 +15,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'
 # Import the Read_Flow function from utils.py
 from utils import Read_Flow
 from utils import list_hdf5_contents
-from utils import get_data
+from utils import get_Eventdata
 from utils import drawEvent
+from utils import Eventdata2Array
 
 if __name__=='__main__':
     root_path = '../../flowdata'
@@ -24,7 +25,9 @@ if __name__=='__main__':
     
     # Use the Read_Flow function to read the charge data
     flow_data = Read_Flow(root_path=root_path, filename=filename)
-    # list_hdf5_contents('/'.join([root_path, filename]))
-    calib_data = get_data(flow_data, 481)
-    print(calib_data.dtype)
-    drawEvent(calib_data)
+    list_hdf5_contents('/'.join([root_path, filename]))
+    # evtID = 481
+    # calib_data = get_Eventdata(flow_data, evtID)
+    # arr_calib = Eventdata2Array(evtdata=calib_data, evtid=evtID)
+    # drawEvent(arr_calib)
+    # print(arr_calib[['x', 'y', 'z']])

84663c89af18d5d19573fdccae953895570b2537
fanrado <radofana@gmail.com>
2024-12-26 01:35:40 -0500

    Added the functions: -  to get the number of events in a file -  to find the principal component in a track using a custom PCA code and also the function from scikit-learn -  to clusterize the hits based on their density using DBSCAN from scikit-learn

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,13 @@
 '''
 Utilities for the analysis.
 '''
+import sys, os
+import matplotlib.pyplot as plt
 import h5flow
 import h5py
 import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
 
 def list_hdf5_contents(file_path):
     with h5py.File(file_path, 'r') as file:
@@ -18,9 +22,14 @@ def Read_Flow(root_path: str, filename: str):
   h5flow_data = h5flow.data.H5FlowDataManager('/'.join([root_path, filename]), 'r')
   return h5flow_data
 
+def getNEvents(h5flow_data):
+    return len(h5flow_data['charge/events', 'charge/calib_prompt_hits'])
+
 def get_Eventdata(h5flow_data, evid: int):
-    calib_data = h5flow_data['charge/events', 'charge/calib_prompt_hits', evid]
-    return calib_data
+    NEvents = getNEvents(h5flow_data=h5flow_data)
+    calib_data = h5flow_data['charge/events', 'charge/calib_prompt_hits', evid][0]
+    calib_data = np.array(calib_data, dtype=calib_data.dtype)
+    return calib_data, NEvents
 
 def Eventdata2Array(evtdata, evtid: int):
     x = evtdata.data['x'].flatten()
@@ -33,7 +42,54 @@ def Eventdata2Array(evtdata, evtid: int):
     arr = np.array(data, dtype=dtype)
     return arr
 
-def drawEvent(arr_evtData):
+def _PCA(data):
+    x = data['x']
+    y = data['y']
+    z = data['z']
+    # covariance matrix
+    data_matrix = np.vstack((x, y, z)).T
+    pca = PCA(n_components=1)
+    pca.fit(data_matrix)
+    mean = pca.mean_
+    principal_component = pca.components_[0]
+    result = np.array([(mean, principal_component)], 
+            dtype=[('mean', '3float64'), ('principal_component', '3float64')])
+    
+    return result
+
+def DBSCAN_clustering(data, eps=1.0, min_samples=5):
+    x = data['x']
+    y = data['y']
+    z = data['z']
+    data_matrix = np.vstack((x, y, z)).T
+    clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(data_matrix)
+
+    return clustering.labels_
+
+def SpatialClustering(data, eps=1.0, min_samples=5):
+    dtype = data.dtype
+    labels = DBSCAN_clustering(data, eps=eps, min_samples=min_samples)
+    # clustering
+    unique_labels = np.unique(labels)
+    print(unique_labels)
+    # tracks = []
+    tracks = np.empty(len(unique_labels), dtype='O')
+    for ilabel, label in enumerate(unique_labels):
+        # if label !=-1:
+        # print(label)
+        selected_indices = np.where(labels == label)[0]
+        mask = np.zeros(len(data['x']), dtype=bool)
+        mask[selected_indices] = True
+        filtered_data = np.zeros(len(data['x'][mask]), dtype=dtype)
+        filtered_data[list(dtype.names)] = np.array(data[list(dtype.names)][mask], dtype=dtype)
+        # tracks.append(filtered_data)
+        tracks[ilabel] = filtered_data
+
+    return tracks
+
+
+########################################################################################
+def drawEvent(arr_evtData, evtID, filename='event'):
     import matplotlib.pyplot as plt
     plt.ioff()
     x = arr_evtData['x'].flatten()
@@ -58,5 +114,38 @@ def drawEvent(arr_evtData):
     ax[2].set_ylabel('z')
     plt.colorbar(sc2, ax=ax[2], label='Charge (Q)')
     # ax.set_aspect('equal', 'box')
-    plt.savefig('event.png')
+    plt.savefig('../output/{}{}.png'.format(filename, evtID))
     plt.close()
+
+# Learning how PCA works
+def custom_PCA(data):
+    x = data['x']
+    y = data['y']
+    z = data['z']
+    # covariance matrix
+    data_matrix = np.vstack((x, y, z)).T
+    covariance_matrix = np.cov(data_matrix, rowvar=False)
+    print("Covariance matrix:\n", covariance_matrix)
+    if np.isnan(covariance_matrix).any():
+        print("Covariance matrix contains NaN values.")
+        return -1
+    # eignvalues and eigenvectors
+    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+    print("Eigenvalues:\n", eigenvalues)
+    print("Eigenvectors:\n", eigenvectors)
+
+    # Calculate the mean of the data points
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    mean_z = np.mean(z)
+    mean = np.array([mean_x, mean_y, mean_z])
+    
+    # Find the principal component
+    principal_component_index = np.argmax(eigenvalues)
+    principal_component = eigenvectors[:, principal_component_index]
+    print("Principal component (eigenvector with the largest eigenvalue):\n", principal_component)
+
+    result = np.array([(mean, principal_component)], 
+            dtype=[('mean', '3float64'), ('principal_component', '3float64')])
+    return result
+########################################################################################

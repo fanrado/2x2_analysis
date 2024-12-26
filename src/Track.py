81736595f583dc2396122adc68ca8a4f7b16dea6
fanrado <radofana@gmail.com>
2024-12-26 01:40:04 -0500

    File to store and analyze tracks. Most of the analysis were written in jupyter notebook of another repository/folder

---
+++ src/Track.py
@@ -0,0 +1,21 @@
+'''
+    Author : radofana@gmail.com
+    Date: December 25, 2024
+
+    - Class Track
+    - Class TrackAnalysis
+'''
+import os
+import sys
+import numpy as np
+
+class Track():
+    def __init__(self, trackArray):
+        self.trakcData = trackArray
+
+    def linearity(self):
+        pass
+
+class TrackAnalysis():
+    def __init__(self):
+        pass
